6d3dd6b80f42a6b70cbe9b25adb90cdaa3204495
Tyra Oberholzer <tyra.n.oberholzer@outlook.com>
2020-08-03 11:43:34 -0400

    updated files

--- Exercises/Exercise Work/TSQL-Ex05-Tyra.sql
+++ Exercises/Exercise Work/TSQL-Ex05-Tyra.sql
@@ -25,9 +25,73 @@
 --6. Bring the data up to date by adding a new row. Use the output clause.
 --7. How many presidents from each state belonged to the various political parties? Aggregate by party
 --and state. Note that this will in eect be a pivot table.
---8. Create a report showing the number of days each president was in oce.
---9. Create a report showing the age (in years) of each present when he took oce.
+--8. Create a report showing the number of days each president was in office.
+ --9. Create a report showing the age (in years) of each present when he took oce.
 --10. See if there is any correlation between a president's party and reported religion, or lack of reported
 --religion.
 
+
 -------------------------------------------------------------------------------------------------------------------------
+
+use TSQLV4;
+
+CREATE TABLE Presidents(
+	ID varchar(20),
+	Last_Name varchar(50), 
+	First_Name varchar(50),
+	Middle_Name varchar(50),
+	Order_of_Presidency varchar(50),
+	Date_of_Birth varchar(50),
+	Date_of_Death varchar(50),
+	Town_Or_County_of_Birth varchar(50),
+	State_of_Birth varchar(50),
+	Home_State varchar(50),
+	Party_Affliliation varchar(50),
+	Date_Took_Office varchar(50),
+	Date_Left_Office varchar(50),
+	Assassination_Attempt varchar(50),
+	Assassinated varchar(50),
+	Religous_Afflilation varchar(50),
+	Image_Path varchar(50)
+);
+
+BULK INSERT Presidents FROM 'C:\Users\Tyra\quantico06\ISTA420\USP.csv'
+WITH
+(
+DATAFILETYPE = 'char',
+FIELDTERMINATOR = ',',
+ROWTERMINATOR = '\n'
+);
+
+ALTER TABLE dbo.Presidents DROP COLUMN Image_Path;
+
+select * from Presidents where ID = 'ID';
+delete from Presidents where ID = 'ID';
+
+alter table dbo.Presidents
+	add IntColumn int identity not null
+Constraint pk_dbo Primary Key (IntColumn);
+
+insert into Presidents values(45, 'Trump', 'Donald', 'John', '45', '1946-06-14', ' ', 'Queens', 'New York', 'New York', 
+'Republican', '2017-01-20', ' ', 'False', 'False', 'Presbyterian');
+go
+;
+
+update dbo.Presidents set Date_Left_Office = '1/20/2009', Assassination_Attempt = 'False', Assassinated = 'False' where Last_Name = 'Obama';
+
+select Home_State, Party_Affliliation, count(*) as counttotal
+from dbo.Presidents
+group by Home_State, Party_Affliliation
+order by counttotal;
+
+select Last_name, First_Name,  datediff(day, Date_Took_Office, Date_Left_Office) as TotNumDays from dbo.Presidents order by ID;
+
+select Last_Name, First_Name, DATEDIFF(year, Date_of_Birth, Date_Took_Office) as NumYears from dbo.Presidents order by ID;
+
+select Religous_Afflilation, Party_Affliliation, count(*) as counttotal
+from dbo.Presidents
+group by Religous_Afflilation, Party_Affliliation
+order by Party_Affliliation;
+
+
+
